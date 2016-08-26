1dc867cc5300498d532d12fd8cfa6ac2829c1203
Zac Shaiken <zacshaiken93@gmail.com>
2016-08-26 12:54:39 -0600

    add secondary keyword functionality

--- js/background.js
+++ js/background.js
@@ -15,6 +15,8 @@ function resetDefaultSuggestion() {
 
 resetDefaultSuggestion();
 
+
+
 chrome.omnibox.onInputCancelled.addListener(function() {
   resetDefaultSuggestion();
 });
@@ -22,8 +24,30 @@ chrome.omnibox.onInputCancelled.addListener(function() {
 chrome.omnibox.onInputEntered.addListener(function(text) {
   	chrome.storage.sync.get('prefSite', function (obj) {
 	  	console.log(obj);
+      if (text.indexOf('s ')==0){
+        sURL='https://google.com/search?q=site:qualtrics.com/support' + text.substring(1,(text.length));
+        chrome.tabs.create({url: sURL });
+      }
+      else if (text.indexOf('kb ')==0){
+        kbURL='http://odo.corp.qualtrics.com/wiki/index.php?search=' + text.substring(3, (text.length));
+        chrome.tabs.create({url: kbURL });
+      }
+      else if (text.indexOf('uid ')==0){
+        uidURL='http://odo.corp.qualtrics.com/?b=ProductToolsUserSearch&autoUser=' + text.substring(4, (text.length));
+        chrome.tabs.create({url: uidURL });
+      }
+      else if (text.indexOf('uem ')==0){
+        uemURL='http://odo.corp.qualtrics.com/?b=ProductToolsUserSearch&autoEmail=' + text.substring(4, (text.length));
+        chrome.tabs.create({url: uemURL });
+      }
+      else if (text.indexOf('sid ')==0){
+        sidURL='http://odo.corp.qualtrics.com/?b=ProductToolsSurveySearch&autoSurvey=' + text.substring(4, (text.length));
+        chrome.tabs.create({url: sidURL });
+      }
+      else {
 	  	var QSearchURL = obj.prefSite + text;
 	  	chrome.tabs.create({url: QSearchURL });
+      }
 	});
 });
 
