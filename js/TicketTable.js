eb36023a2f3df587ffe543ea68e5fb5c14db3f60
Matthew Bloomfield <matthewb@qualtrics.com>
2016-09-14 15:33:02 -0600

    UPdating table to work with all users

--- js/TicketTable.js
+++ js/TicketTable.js
@@ -23,19 +23,16 @@ chrome.storage.sync.get({
 		});
 	}
 
-	if ((window.location.href.indexOf('TicketViewer') == -1) && (window.location.href.indexOf("TopNav=Tickets") >= 0)){
+	if ((window.location.href.indexOf('TicketViewer') == -1) && (((window.location.href.indexOf("a=Tickets") >= 0) && (window.location.href.indexOf("tid")==-1))|| (window.location.href.indexOf("TopNav=Tickets")>=0))){
 		console.log("In the table");
 		var firebaseRef = new Firebase("https://odo-enhanced.firebaseio.com/tickets");
 		function addColumn () {
-			var tbsel = "#9d48ef152aea2424d4a555b4c702e23e6"
-			var tbselhead = tbsel + " > thead > tr"
-			$(tbselhead).append('<td class="TicketDescription sorting" tabindex="0" aria-controls="9d48ef152aea2424d4a555b4c702e23e6" rowspan="1" colspan="1" aria-label="Description: activate to sort column ascending">Ticket Owner</td>');
-
-			var tblength = $(tbsel).find('tr').length;
+			$('table').eq(1).find("thead > tr").append('<th class="TicketDescription sorting" tabindex="0" aria-controls="9d48ef152aea2424d4a555b4c702e23e6" rowspan="1" colspan="1" aria-label="Description: activate to sort column ascending">Ticket Owner</th>');
+			var tblength = $('table').eq(1).find('tr').length;
 			firebaseRef.once("value", function(snapshot) {
 			    firebaseTickets = snapshot.val();
 				for (i=1; i<=tblength; i++){
-					var tid = $(tbsel + " > tbody > tr:nth-child("+i+") > td:nth-child(7) > a").attr('ticketid');
+					var tid =$('table').eq(1).find("tbody > tr:nth-child("+i+") > td:nth-child(7) > a").attr('ticketid');
 					if (firebaseTickets[tid]) {
 						var owner = firebaseTickets[tid]["owner"];
 						if (firebaseTickets[tid]["eid"] == EmployeeID){
@@ -44,7 +41,7 @@ chrome.storage.sync.get({
 					} else {
 						owner = 'You';
 					}
-					$(tbsel + " > tbody > tr:nth-child("+i+")").append("<td><strong>"+ owner +"</strong></td>");
+					$('table').eq(1).find("tbody > tr:nth-child("+i+")").append("<td><strong>"+ owner +"</strong></td>");
 				}
 			});
 
