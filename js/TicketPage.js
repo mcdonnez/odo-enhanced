b21d43f00d05d9dc8e050afecba03f1ae4f9db85
Matthew Bloomfield <matthewb@qualtrics.com>
2016-09-21 12:35:55 -0600

    Removing any alerts

--- js/TicketPage.js
+++ js/TicketPage.js
@@ -7,8 +7,6 @@ chrome.storage.sync.get({
 }, function(items) {
 	EmployeeID = items.eid;
 	EmployeeName = items.ename;
-	console.log(EmployeeID);
-	console.log(EmployeeName);
 
 
 	if (window.location.href.indexOf('TicketViewer') >= 0) {
@@ -20,41 +18,15 @@ chrome.storage.sync.get({
 			});
 		}
 		if (!EmployeeName) {
-			var EmployeeName = null;
-			// chrome.storage.sync.set({
-			// 	ename: EmployeeName
-			// });
+			var EmployeeName = userData.employeeName;
+			chrome.storage.sync.set({
+				ename: EmployeeName
+			});
 		}
 
 		console.log("In the Ticket Page");
 		var firebaseRef = new Firebase("https://odo-enhanced.firebaseio.com/tickets");
 		var tid = window.location.href.split('tid=')[1];
-		function setCookie(cname, cvalue, exdays) {
-		    if (exdays=="") {
-		    	expires = "";
-		    }
-		    else {
-		    	var d = new Date();
-		    	d.setTime(d.getTime() + (exdays*24*60*60*1000));
-		    	var expires = "expires="+ d.toUTCString();
-		    }
-		    document.cookie = cname + "=" + cvalue + "; " + expires;
-		}
-
-		function getCookie(cname) {
-		    var name = cname + "=";
-		    var ca = document.cookie.split(';');
-		    for(var i = 0; i <ca.length; i++) {
-		        var c = ca[i];
-		        while (c.charAt(0)==' ') {
-		            c = c.substring(1);
-		        }
-		        if (c.indexOf(name) == 0) {
-		            return c.substring(name.length,c.length);
-		        }
-		    }
-		    return "";
-		}
 
 		function createEventListener(eid,empname) {
 			$("#owner-"+eid).bind('click',function(){MCclick(eid,empname)});
@@ -71,34 +43,18 @@ chrome.storage.sync.get({
 			var tblength = $(tbsel + ' > tbody').find('tr').length;
 			var owners = [];
 			for (i = 1; i <= tblength; i++) {
-				//var tbnum = i;
-				var rowsel = tbsel + " > tbody > tr:nth-child(" + i + ") > td:nth-child(1)";
+					var rowsel = tbsel + " > tbody > tr:nth-child(" + i + ") > td:nth-child(1)";
 				var fulname = $(rowsel).text();
 				var empname = fulname.slice(6,fulname.length - 5);
 				var eid = $(rowsel).find("a").attr("href").split("eid=")[1];
 				owners.push(eid);
-				//var funcpair = "'" + i + "','" + empname + "'";
 				$(rowsel).prepend('<input id="owner-' + eid + '" type="radio" name="owner">&nbsp;');
-				//taken out: onclick="MCclick('+funcpair+')"
 				createEventListener(eid,empname);
-				//console.log("$('"+rowsel+" > input').bind('click',function(){MCclick('"+tbnum.toString()+"','"+empname+"')});")
 			}
-			//$("#owner-1").bind('click',function(){MCclick("1","ML")});
-			//$("#owner-2").bind('click',function(){MCclick("2","TS")});
 			return 'MCscreated'
 		}
 
 		function MCclick(eid,empname){
-			// var oldcookie = getTicketOwner(tid);
-			// if (oldcookie != ""){
-				// var oldrownum = oldcookie.split('-')[1];
-				// console.log("old row " + oldrownum);
-				// console.log("new row " + tbrow);
-				// var oldrow = '#RightMenuColumn > table.Orange > tbody > tr:nth-child(' + oldrownum + ') > td:nth-child(1)';
-				// removeOwnerTag(oldrow);
-			// }
-
-
 			//HTML
 			removeOwnerTag();
 			addOwnerTag(eid);
@@ -112,15 +68,10 @@ chrome.storage.sync.get({
 		}
 
 		function removeOwnerTag(){
-			// var fontsel = rowsel + ' > span';
-			// $(fontsel).remove();
 			$(".owner-specification").remove();
 		}
 
 		function editTicketOwner(eid,empname){
-			// var newcookie = empname+"-"+tbrow;
-			// console.log('newcookie');
-			// setCookie(tid,newcookie,7);
 			var obj = {
 				id: tid,
 				owner: empname,
