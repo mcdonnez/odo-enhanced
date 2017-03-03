2944058573aadc8bfcdafbc3ad410571068f32eb
Zachary McDonnell <mailman966@gmail.com>
2017-03-03 09:31:19 -0700

    fix syntax issues as well as console errors since we expanded the domains for the extension

--- js/TicketTable.js
+++ js/TicketTable.js
@@ -1,19 +1,19 @@
 chrome.storage.sync.get({
 	eid: "",
 	ename: "",
-	showQuniTickets: ""//Quni Ticket Breakdown
-}, function (items) {
+	showQuniTickets: "" //Quni Ticket Breakdown
+}, function(items) {
 	var EmployeeID = items.eid;
 	var EmployeeName = items.ename;
 	var showQuniTickets = items.showQuniTickets;
 
-	if (!EmployeeID) {
+	if (!EmployeeID && userData) {
 		EmployeeID = userData.employeeID;
 		chrome.storage.sync.set({
 			eid: EmployeeID
 		});
 	}
-	if (!EmployeeName) {
+	if (!EmployeeName && userData) {
 		EmployeeName = userData.employeeName;
 		chrome.storage.sync.set({
 			ename: EmployeeName
@@ -22,21 +22,20 @@ chrome.storage.sync.get({
 	var tableIndex;
 	if (showQuniTickets) {
 		tableIndex = 2;
-	}
-	else {
+	} else {
 		tableIndex = 1;
 	}
 
 	var href = window.location.href;
-	if (href.indexOf('TicketViewer') == -1 && href.indexOf('SupportQualifications') == -1
-		&& ((href.indexOf("a=Tickets") >= 0 && href.indexOf("tid") == -1) || href.indexOf("TopNav=Tickets") >= 0)) {
+	if (href.indexOf('TicketViewer') == -1 && href.indexOf('SupportQualifications') == -1 &&
+		((href.indexOf("a=Tickets") >= 0 && href.indexOf("tid") == -1) || href.indexOf("TopNav=Tickets") >= 0)) {
 		var firebaseRef = new Firebase("https://odo-enhanced.firebaseio.com/tickets");
 
 		function addColumn() {
 			var $table = $('table');
 			$table.eq(tableIndex).find("thead > tr").append('<th class="TicketDescription sorting" tabindex="0" aria-controls="9d48ef152aea2424d4a555b4c702e23e6" rowspan="1" colspan="1" aria-label="Description: activate to sort column ascending">Ticket Owner</th>');
 			var tblength = $table.eq(tableIndex).find('tr').length;
-			firebaseRef.once("value", function (snapshot) {
+			firebaseRef.once("value", function(snapshot) {
 				var firebaseTickets = snapshot.val();
 				for (i = 1; i <= tblength; i++) {
 					var tid = $table.eq(tableIndex).find("tbody > tr:nth-child(" + i + ") > td:nth-child(7) > a").attr('ticketid');
@@ -54,7 +53,7 @@ chrome.storage.sync.get({
 
 		}
 
-		window.setTimeout(function () {
+		window.setTimeout(function() {
 			addColumn();
 		}, 200);
 
