21b771b5577ab4fe009f699680893ee89ef28caf
Andrew Terry <andrewt@qualtrics.com>
2017-12-11 15:03:45 -0700

    Creating the new MBA functionality
    Edited the Options HTML and js to enter employee ID. Updated manifest for API permissions. MBA.js to create buttons and gather necesary info. Background runs API calls to get userID, proxy log in, and post to Slack

--- js/background.js
+++ js/background.js
@@ -68,3 +68,183 @@ chrome.omnibox.onInputEntered.addListener(function (text) {
 	});
 });
 
+chrome.runtime.onMessage.addListener(
+	function (request, sender) {
+		if (request.msg === "myaccount_clicked" || request.msg === "promote_clicked") {
+			var datacenter = request.data.datacenter;
+			var ticketID = request.data.ticketID;
+			console.log("message received");
+			chrome.storage.sync.get("eid", function (obj) {
+				var userID = obj.eid;
+				$(document).ready(function () {
+					getUserList(datacenter, userID, request.msg, ticketID);
+				});
+			});
+		};
+	});
+
+
+function makePersonalUrl(dc, qualtricsID, ticketID) {
+	if (dc == "CO1") {
+		chrome.tabs.create({ "url": "https://odo.corp.qualtrics.com/?a=ResearchSuite&b=RSUserAccountAccess&bid=qus1&uid=" + qualtricsID + "&RSLogin&FromTicket=" + ticketID + "&Direct" });
+	}
+	else if (dc === "AZ1") {
+		chrome.tabs.create({ "url": "https://odo.corp.qualtrics.com/?a=ResearchSuite&b=RSUserAccountAccess&bid=qaz1&uid=" + qualtricsID + "&RSLogin&FromTicket=" + ticketID + "&Direct" });
+	}
+	else if (dc === "CA1") {
+		chrome.tabs.create({ "url": "https://odo.corp.qualtrics.com/?a=ResearchSuite&b=RSUserAccountAccess&bid=qcorp&uid=" + qualtricsID + "&RSLogin&FromTicket=" + ticketID + "&Direct" });
+	}
+	else if (dc === "EU1") {
+		chrome.tabs.create({ "url": "https://odo.corp.qualtrics.com/?a=ResearchSuite&b=RSUserAccountAccess&bid=qeurope&uid=" + qualtricsID + "&RSLogin&FromTicket=" + ticketID + "&Direct" });
+	}
+	else if (dc === "AU1") {
+		chrome.tabs.create({ "url": "https://odo.corp.qualtrics.com/?a=ResearchSuite&b=RSUserAccountAccess&bid=qasia&uid=" + qualtricsID + "&RSLogin&FromTicket=" + ticketID + "&Direct" });
+	}
+}
+
+function getUserList(datacenter, userID, msg, ticketID) {
+	var requestURL = '';
+	var APItoken = '';
+	var qualtricsID = localStorage['qualtricsID' + datacenter] || '';
+	switch (datacenter) {
+		case 'CA1':
+			requestURL = "https://ca1.qualtrics.com/API/v3/users";
+			APItoken = "DN0IZVvgSpGxWA6NZCbBX05z4IucmDdx3hxIsk3P";
+			break;
+		case 'CO1':
+			requestURL = "https://co1.qualtrics.com/API/v3/users";
+			APItoken = "rp0jgST3Lg2iTCE2xZpvsSKoMJTnyULOlUTdFuoW";
+			break;
+		case 'AZ1':
+			requestURL = "https://az1.qualtrics.com/API/v3/users";
+			APItoken = "KbdFL5Lces6qzpQqkv27hwEMnqBmnR4foskSZREe";
+			break;
+		case 'AU1':
+			requestURL = "https://au1.qualtrics.com/API/v3/users";
+			APItoken = "9hTIVdvyVTbnLvb7YTqXDwFvLLm14DZxTlqx2ueI";
+			break;
+		case 'EU1':
+			requestURL = "https://eu1.qualtrics.com/API/v3/users";
+			APItoken = "v1L62gZaaO2zIFEZIfdohUrEKydT5cIVsawHxnC7";
+			break;
+		default:
+			console.log('Not a valid datacenter - cannot generate request url');
+			return 'error';
+	}
+	if (qualtricsID === '') {
+		makeCall(requestURL, APItoken, userID, datacenter, msg, ticketID);
+	}
+	else if (msg === 'myaccount_clicked') {
+		makePersonalUrl(datacenter, qualtricsID, ticketID);
+	}
+	else if (msg === 'promote_clicked') {
+		promoteMBA(qualtricsID, userID, datacenter);
+	}
+	else { console.log("What do you want me to do!?") };
+
+
+
+}
+
+function makeCall(url, token, userID, datacenter, msg, ticketID) {
+
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": url,
+		"method": "GET",
+		"headers": {
+			"x-api-token": token,
+			"cache-control": "no-cache",
+		}
+	}
+
+	$.ajax(settings).done(function (response) {
+		var listing = response.result.elements;
+		var nextPage = response.result.nextPage;
+		var qualtricsID = '';
+		var employeeID = userID + "+" + datacenter + "@qualtrics.com";
+		for (var i = 0; i < listing.length; ++i) {
+			if (listing[i].username.toLowerCase() === employeeID.toLowerCase()) {
+				qualtricsID = listing[i].id;
+				console.log(qualtricsID);
+			}
+		}
+		if (qualtricsID !== '') {
+			localStorage['qualtricsID' + datacenter] = qualtricsID;
+			if (msg === 'myaccount_clicked') {
+				makePersonalUrl(datacenter, qualtricsID, ticketID);
+			}
+			else if (msg === 'promote_clicked') {
+				promoteMBA(qualtricsID, userID, datacenter);
+			}
+			else { console.log("What do you want me to do!?2") }
+		}
+		else if (nextPage !== null) {
+			makeCall(nextPage, token, userID, datacenter, msg, ticketID);
+		}
+		else {
+			console.log("userID not found");
+		}
+	});
+}
+
+function promoteMBA(qualtricsID, userID, datacenter) {
+	var baseUrl = "http://odo.corp.qualtrics.com/?b=RSUserProfile&UserAccountPromotion&uid=" + qualtricsID + "&hours=1&reason=";
+	var reason = prompt("Please enter the reason", "Extension Testing");
+	var url = baseUrl + reason;
+	var userURL = '';
+	switch (datacenter) {
+		case 'CA1':
+			userURL = "https://odo.corp.qualtrics.com/?a=ResearchSuite&b=RSUserProfile&bid=qcorp&uid=" + qualtricsID;
+			break;
+		case 'CO1':
+			userURL = "https://odo.corp.qualtrics.com/?a=ResearchSuite&b=RSUserProfile&bid=qus1&uid=" + qualtricsID;
+			break;
+		case 'AZ1':
+			userURL = "https://odo.corp.qualtrics.com/?a=ResearchSuite&b=RSUserProfile&bid=qaz1&uid=" + qualtricsID;
+			break;
+		case 'AU1':
+			userURL = "https://odo.corp.qualtrics.com/?a=ResearchSuite&b=RSUserProfile&bid=qasia&uid=" + qualtricsID;
+			break;
+		case 'EU1':
+			userURL = "https://odo.corp.qualtrics.com/?a=ResearchSuite&b=RSUserProfile&bid=qeurope&uid=" + qualtricsID;
+			break;
+		default:
+			console.log('Not a valid datacenter - cannot generate request url');
+			return 'error';
+	}
+
+	var settings = {
+		"url": url
+	};
+	var newURL = "https://odo.corp.qualtrics.com/?b=RSUserProfile&UserAccountPromotion&uid=" + qualtricsID + "&hours=1&reason=" + reason;
+	$.ajax(settings).done(function (response) {
+		var Responses= response;
+		response.result;
+		console.log(Responses);
+		var test= response.error;
+		if ( typeof test != 'undefined') {
+			console.log("MBA promotion failed");
+			postToSlack(userID, newURL, reason);
+		}
+	});
+}
+
+function postToSlack(userID, url, reason) {
+	if (reason !== '') {
+		payload = JSON.stringify({ "text": "@mbagrantors \n" + userID + " would like to have MBA for " + reason + "\n<" + url + ">" });
+	}
+	var settings = {
+		"url": "https://hooks.slack.com/services/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",
+		"username": "MBA Request",
+		"icon_url": "https://login.qualtrics.com/ControlPanel/Graphic.php?IM=IM_0wTaUBXzr2VM9ed",
+		"data": payload,
+		"dataType": "json",
+		"processData": "false",
+		"type": "POST"
+	}
+	$.ajax(settings).done(function (response) {
+		console.log(response.text);
+	});
+}
